2f0631a2a1eff8dab0998395b6d627b75da049e5
solarpark7346 <solarpark7346@gmail.com>
2023-03-25 00:18:47 +0900

    add: CustomUserDetails 추가

---
+++ src/main/java/com/develop/web/dto/CustomUserDetails.java
@@ -0,0 +1,51 @@
+package com.develop.web.dto;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+
+@Component
+public class CustomUserDetails implements UserDetails {
+
+    private static final long serialVersionUID = 1L;
+
+    private String userid;
+    private String userPassword;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.userPassword;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userid;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
