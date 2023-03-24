378a83666be7b47f8d087326424f304e4bfa0c1b
solarpark7346 <solarpark7346@gmail.com>
2023-03-25 00:15:44 +0900

    add: SecurityConfig 설정 추가

---
+++ src/main/java/com/develop/web/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.develop.web.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+/**
+ * Spring Security 사용을 위한 Configuration Class를 작성하기 위해서
+ * WebSecurityConfigurerAdapter를 상속하여 클래스를 생성하고
+ * @Configuration 애노테이션 대신 @EnableWebSecurity 애노테이션을 추가한다.
+ */
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable() // post 방식으로 값을 전송할 때 token을 사용해야하는 보안 설정을 해제
+            .authorizeRequests()
+            .antMatchers("/").permitAll()
+            .antMatchers("/home").hasRole("USER")
+            .anyRequest()
+            .authenticated();
+        http
+            .formLogin()
+            .loginPage("/")
+            .loginProcessingUrl("/auth/login")
+            .usernameParameter("userid")
+            .passwordParameter("userPassword");
+    }
+}
